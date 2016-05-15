4e66954a79d96728d0a6a618e16ea88a50d061cf
Stuart Simpson <stuart@pixelandline.com>
2016-05-15 01:01:55 -0600

    -added actual icons and mocked out category data

--- source/controllers/SelectSubCategoryCtrl.js
+++ source/controllers/SelectSubCategoryCtrl.js
@@ -2,6 +2,7 @@ angular.module('MyApp')
     .controller('SelectSubCategoryCtrl', function($scope, $http, toastr, $stateParams) {
         var ctrl = this;
         ctrl.selectedCategory = $stateParams.category;
+        ctrl.subCategories = [{"id": 1, "name": "Electronics", "icon": "ion-ios-monitor"}, {"id": 1, "name": "Games", "icon": "ion-ios-game-controller-b"}, {"id": 1, "name": "Vehicles", "icon": "ion-model-s"}, {"id": 1, "name": "Books", "icon": "ion-ios-book"}];
         getSubCategories();
 
         //gets list of devices
