5d96fe4d753e36ff9bcf844de91fb19d28ee30b0
Stuart Simpson <stuart@pixelandline.com>
2016-05-29 02:32:45 -0600

    -added listing detail html and css

--- source/controllers/ListingDetailCtrl.js
+++ source/controllers/ListingDetailCtrl.js
@@ -1,5 +1,6 @@
 angular.module('MyApp')
     .controller('ListingDetailCtrl', function($scope, $http, $state) {
         var ctrl = this;
+        ctrl.listing = {"title": "Looking for used GalaxyS 7", "post_date": "5/17/2016", "location": "Boise, Idaho"};
         //add logic to get listing details
     });
